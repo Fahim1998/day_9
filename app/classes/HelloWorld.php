9dea07d868acb181ecf2c55ebfe9c71b2b5cb057
Faahim1998 <fahimbsc57@gmail.com>
2022-01-27 13:11:32 +0600

    done

---
+++ app/classes/HelloWorld.php
@@ -0,0 +1,32 @@
+<?php
+namespace App\classes;
+class HelloWorld
+{
+    public $name = 'BITM';
+
+   public function index()
+   {
+
+
+      echo $this->name;
+   }
+
+}
+
+// variable
+// operator
+// statement
+// array
+//3 major rules for variable
+/*
+ * start with $,
+ * a-z A-z 0-9
+ * no number in first
+ */
+
+/*
+ * 3 standerd rules for variable
+ * small latter
+ * meaning full
+ * readable
+ */
